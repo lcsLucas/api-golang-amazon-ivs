adeef7d23474c6e8c434956199fe2960219d37f1
Lucas Rosa <lucasrosa@anglopp.g12.br>
2023-03-29 10:38:58 -0300

    Refazendo o projeto no padrão de microsserviços

--- api/routes/routes.go
+++
@@ -1,9 +0,0 @@
-package routes
-
-import "github.com/gofiber/fiber/v2"
-
-var App *fiber.App
-
-func InicializeRoutes() {
-	InicializeRoutesIVS()
-}
--- utils/http_response.go
+++
@@ -1,5 +0,0 @@
-package utils
-
-type ResponseError struct {
-	Message string `json:"error"`
-}
